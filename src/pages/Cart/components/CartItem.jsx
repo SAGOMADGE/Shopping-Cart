7706cfd51786d83595308b3ea7549e246a95fccc
SAGOMADGE <temokishmariya@gmail.com>
2026-03-11 22:08:13 +0300

    style: implement basic styles for CartItem

--- src/pages/Cart/components/CartItem.jsx
+++ src/pages/Cart/components/CartItem.jsx
@@ -3,30 +3,36 @@ import styles from './CartItem.module.css';
 const CartItem = ({ item, removeFromCart, updateQuantity }) => {
   return (
     <div className={styles.cartItem}>
-      <img src={item.image} alt={item.title} className={styles.image} />
+      <div className={styles.imgWrapper}>
+        <img src={item.image} alt={item.title} className={styles.image} />
+      </div>
 
-      <p className={styles.title}>{item.title}</p>
-      <p className={styles.price}>{item.price}</p>
-      <p className={styles.quantity}>{item.quantity}</p>
+      <div className={styles.info}>
+        <p className={styles.title}>{item.title}</p>
+        <p className={styles.price}>{item.price}</p>
+
+        <div className={styles.controls}>
+          <button
+            className={styles.subtractBtn}
+            onClick={() => updateQuantity(item.id, -1)}
+          >
+            -
+          </button>
+          <span className={styles.quantity}>{item.quantity}</span>
+          <button
+            className={styles.addBtn}
+            onClick={() => updateQuantity(item.id, +1)}
+          >
+            +
+          </button>
+        </div>
+      </div>
 
       <button
-        className={styles.substractBtn}
-        onClick={() => updateQuantity(item.id, -1)}
-      >
-        -
-      </button>
-      <button
-        className={styles.addBtn}
-        onClick={() => updateQuantity(item.id, +1)}
-      >
-        +
-      </button>
-
-      <button
-        className={styles.removeFromCart}
+        className={styles.removeBtn}
         onClick={() => removeFromCart(item.id)}
       >
-        удалить товар
+        x
       </button>
     </div>
   );
