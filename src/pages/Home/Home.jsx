790a78dcef84ec965a78da27fcbc38acaa23085f
SAGOMADGE <temokishmariya@gmail.com>
2026-03-12 05:31:40 +0300

    style: implement Home page hero section with lucide icon

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,18 @@
+import { NavLink } from 'react-router-dom';
+import styles from './Home.module.css';
+import { ShoppingBag } from 'lucide-react';
+
 const Home = () => {
-  return 'Hey Home';
+  return (
+    <div className={styles.homePage}>
+      <ShoppingBag size={64} color="var(--primary)" />
+      <h1 className={styles.homeHeadline}>Fake Store</h1>
+      <h2 className={styles.homeDescription}>Лучшие цены</h2>
+      <NavLink to="/shop" end className={styles.homeToShopLink}>
+        В магазин
+      </NavLink>
+    </div>
+  );
 };
 
 export default Home;
